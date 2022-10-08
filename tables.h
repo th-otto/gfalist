68894627bd3e3bdf50b15ad652b4901b5defc5ea
Thorsten Otto <admin@tho-otto.de>
2022-10-08 18:39:26 +0200

    Add some line commands

--- tables.h
+++ tables.h
@@ -1,7 +1,7 @@
 #ifndef TABLES_H
 #define TABLES_H
 
-extern const char *const gfalct[512];
+extern const char *const gfalct[520];
 extern const char *const gfapft[256];
 extern const char *const gfasft_208[256];
 extern const char *const gfasft_209[256];
